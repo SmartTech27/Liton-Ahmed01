6af7c4df91cc1cda338ca52170c392d042975fec
liton <liton@10.0.0.199>
2022-07-09 12:52:17 -0500

    My First Project in GitHub.

---
+++ src/test/java/inheritance01/GithubTesting.java
@@ -0,0 +1,11 @@
+package inheritance01;
+
+public class GithubTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hello Guys, Testing for Git Hub Push");
+	}
+
+}
