2a18978412d90dba73e55dad985df9ce12f458aa
liton <liton@10.0.0.199>
2022-07-09 14:43:18 -0500

    Updated date: 07.09.2022

--- src/test/java/inheritance01/GithubTesting.java
+++ src/test/java/inheritance01/GithubTesting.java
@@ -6,6 +6,7 @@ public class GithubTesting {
 		// TODO Auto-generated method stub
 
 		System.out.println(" Hello Guys, Testing for Git Hub Push");
+		System.out.println(" I updated my project today: Date 07/09/2022. *Eid Mubarak everybody*");
 	}
 
 }
